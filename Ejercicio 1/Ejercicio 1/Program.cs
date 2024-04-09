832f3db7704332becd3e8e9406b2dd53c452139d
TrinidadBonzi <trinidad.bonzi.ices@gmail.com>
2024-04-09 17:12:21 -0300

    Primer commit
    Primer commit donde pegamos los ejercicios de la clase pasada.

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Calcular la suma de dos números.
+Console.Write("Ingrese un número: ");
+int numero1 = Int32.Parse(Console.ReadLine());
+Console.Write("Ingrese un número: ");
+int numero2 = Int32.Parse(Console.ReadLine());
+
+int suma = numero1 + numero2;
+Console.Write("El resultado de la suma es: " + suma);
